b75a83c6e53ee6a89ced31ded7a2002def9ec637
Roman Kievskiy <roman.aka.loki@gmail.com>
2016-10-16 16:00:15 +0300

    simply work

--- src/dataset4birttester/Java4BIRTTester.java
+++ src/dataset4birttester/Java4BIRTTester.java
@@ -9,10 +9,15 @@ public class Java4BIRTTester {
     public static void main(String[] args) throws SQLException {
         Dataset ds = null;
         try {
-            ds = new Dataset("jdbc:extendedsystems:advantage://srv-a10:6262/y$/fabius/reflis/dict.add", "AdsSys", "");
-            if (ds.select("select 1 as one from system.iota union all select 2 as one from system.iota")) {
+            // PostgreSQL
+            ds = new Dataset("jdbc:postgresql://localhost:5432/test", "user", "123");
+            // ADS
+            //ds = new Dataset("jdbc:extendedsystems:advantage://srv-a10:6262/y$/fabius/reflis/dict.add", "AdsSys", "");
+            //if (ds.select("select 1 as one from system.iota union all select 2 as one from system.iota")) {
+            // PostgreSQL
+            if (ds.select("select 1 as one union all select 2")) {
                 while (ds.next()) {
-                    System.out.println(ds.field("one"));
+                    System.out.println(Integer.toString(ds.field("one").asInteger()));
                 }
             }
         } catch (Exception error) {
